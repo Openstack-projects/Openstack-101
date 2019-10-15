552ef430dd607c1700a07a1b49277f1604f25467
Pratik Raj <Rajpratik71@gmail.com>
2019-10-15 18:56:16 +0530

    Create delete_compute_node_by_name.py

---
+++ delete_compute_node_by_name.py
@@ -0,0 +1,12 @@
+from openstack import connection
+
+conn = connection.Connection(
+        auth_url = 'http://YOUR_floating_IP:5000/v2.0',
+        username = 'YOUR_USERNAME',
+        password = 'YOUR_PASSWORD',
+        project_name = 'YOUR_PROJECT_NAAME')
+        
+
+srv = conn.compute.find_server("MyServer1")
+
+conn.compute.delete_server(srv, ignore_missing=True)
